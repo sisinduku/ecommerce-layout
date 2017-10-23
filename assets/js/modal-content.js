19a6e4bfab01b0561d4df6a64ab9642ceea2d347
sisinduku <saptanto.sindu@gmail.com>
2017-10-23 21:52:38 +0700

    Add Front End E-commerce

---
+++ assets/js/modal-content.js
@@ -0,0 +1,90 @@
+const chartModal = `
+<div class="content">
+  <div class="ui three stackable cards">
+    <div class="card">
+      <div class="content">
+        <img class="right floated mini ui image" src="https://prominic.net/wp-content/themes/hoxa/assets/images/default.jpg">
+        <div class="header">
+          Jeans Afro Black
+        </div>
+        <div class="meta">
+          $ 30
+        </div>
+        <div class="description">
+          Qty:
+          <div class="ui input">
+            <input placeholder="qty" type="number" min="1" value="1">
+          </div>
+        </div>
+        <div class="description">
+          Jeans Afro Premium Pre Washed
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui red button">Cancel</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <img class="right floated mini ui image" src="https://prominic.net/wp-content/themes/hoxa/assets/images/default.jpg">
+        <div class="header">
+          Jeans Afro Black
+        </div>
+        <div class="meta">
+          $ 30
+        </div>
+        <div class="description">
+          Qty:
+          <div class="ui input">
+            <input placeholder="qty" type="number" min="1" value="1">
+          </div>
+        </div>
+        <div class="description">
+          Jeans Afro Premium Pre Washed
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui red button">Cancel</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <img class="right floated mini ui image" src="https://prominic.net/wp-content/themes/hoxa/assets/images/default.jpg">
+        <div class="header">
+          Jeans Afro Black
+        </div>
+        <div class="meta">
+          $ 30
+        </div>
+        <div class="description">
+          Qty:
+          <div class="ui input">
+            <input placeholder="qty" type="number" min="1" value="1">
+          </div>
+        </div>
+        <div class="description">
+          Jeans Afro Premium Pre Washed
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui red button">Cancel</div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<div class="actions">
+  <div class="ui positive right labeled icon button">
+    Yep, Continue to complete
+    <i class="checkmark icon"></i>
+  </div>
+</div>
+`
+const shippingModal = `
+<div class="content">
+  <div class="ui raised segment">
+    <span class="check circle icon"></span>
+    <p>Your order has been saved.</p>
+  </div>
+</div>
+`
---
+++ assets/js/product-content.js
@@ -0,0 +1,72 @@
+const productContent = `
+<div class="sixteen wide column">
+  <div class="ui link three stackable cards">
+    <div class="card">
+      <div class="image">
+        <img src="https://ae01.alicdn.com/kf/HTB1gPXydE3IL1JjSZFMq6yjrFXaC/New-Arrival-Spring-Men-s-Jackets-Solid-Fashion-Coats-Male-Casual-Slim-Stand-Collar-Bomber-Jacket.jpg_640x640.jpg" alt="product">
+      </div>
+      <div class="content">
+        <div class="header">Matt Giampietro</div>
+        <div class="meta">
+          <a>Price: Rp. 1.200.000</a>
+        </div>
+        <div class="description">
+          Cow Leather pre washed processing
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui vertical animated button" id="button-id" tabindex="0">
+          <div class="hidden content">Shop</div>
+          <div class="visible content">
+            <i class="shop icon"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://ae01.alicdn.com/kf/HTB1gPXydE3IL1JjSZFMq6yjrFXaC/New-Arrival-Spring-Men-s-Jackets-Solid-Fashion-Coats-Male-Casual-Slim-Stand-Collar-Bomber-Jacket.jpg_640x640.jpg" alt="product">
+      </div>
+      <div class="content">
+        <div class="header">Matt Giampietro</div>
+        <div class="meta">
+          <a>Price: Rp. 1.200.000</a>
+        </div>
+        <div class="description">
+          Cow Leather pre washed processing
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui vertical animated button" id="button-id" tabindex="0">
+          <div class="hidden content">Shop</div>
+          <div class="visible content">
+            <i class="shop icon"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://ae01.alicdn.com/kf/HTB1gPXydE3IL1JjSZFMq6yjrFXaC/New-Arrival-Spring-Men-s-Jackets-Solid-Fashion-Coats-Male-Casual-Slim-Stand-Collar-Bomber-Jacket.jpg_640x640.jpg" alt="product">
+      </div>
+      <div class="content">
+        <div class="header">Matt Giampietro</div>
+        <div class="meta">
+          <a>Price: Rp. 1.200.000</a>
+        </div>
+        <div class="description">
+          Cow Leather pre washed processing
+        </div>
+      </div>
+      <div class="extra content">
+        <div class="ui vertical animated button" id="button-id" tabindex="0">
+          <div class="hidden content">Shop</div>
+          <div class="visible content">
+            <i class="shop icon"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`
